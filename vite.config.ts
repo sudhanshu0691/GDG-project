e731e992a46f95423928367b60e107afd0231ff4
sudhanshu0691 <sudhanshu0691.s@gmail.com>
2025-09-04 00:09:52 +0530

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/GDG-project/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
