ba1f5d84aaf2f98a55486bfdf14b6e2e6f0ceb59
sudhanshu0691 <sudhanshu0691.s@gmail.com>
2025-09-04 13:51:55 +0530

    i am changing the url link

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GDG-project/',
+  base: '/gdgoncampus-ggct/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
